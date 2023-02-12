fecfbf30af62e9e15e08a90d3a3c7901e6385764
董泽平 <dzp19903@163.com>
2023-02-12 15:47:46 +0800

    feat: 按钮组件完成开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import './App.css';
+import React from "react";
+
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-      app
+      <div>button测试</div>
+      <Button>dzp</Button>
+      <Button size="large">dzp</Button>
+      <Button size="small">dzp</Button>
+
+      <Button type="primary" size="large">dzp</Button>
+      <Button type="danger">dzp</Button>
+      <Button type="link" href="http://www.baidu.com" onClick={()=>{console.log('a')}}>dzp</Button>
+      <Button disabled={true}>dzp</Button>
+      <Button onClick={()=>console.log('click')}>惦记我</Button>
     </div>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,46 @@
-
-const Button = () => {
+import React from "react"
+import classNames from "classnames"
+interface ButtonProps {
+  type:'primary' | 'danger'  | 'link' | 'default' //类型
+  size:'large' | 'small' | 'middle' //尺寸
+  classname:string //class
+  disabled:Boolean //禁用选项
+  children:React.ReactNode
+  href:string //链接地址
+}
+type ButtonUnin = ButtonProps & React.BaseHTMLAttributes<HTMLElement> 
+type ButtonUnion =ButtonUnin & React.AnchorHTMLAttributes<HTMLElement>
+type ButtonPropsResult = Partial<ButtonUnion>
+const Button = (props:ButtonPropsResult) => {
+  const { type,size,classname,disabled,children,href,...resetProps } = props;
+  const classes = classNames('btn',classname,{
+    [`btn-${type}`]:type,
+    [`btn-${size}`]:size,
+    [`btn-disabled`]:disabled
+  });
+  if(type && type === 'link') {
+    return (
+      <a 
+        {...resetProps} 
+        href={href}>
+        {children}
+      </a>
+    )
+    
+  }
   return (
-    <div>button</div>
+    <button
+      className={classes}
+      {...resetProps}
+    >
+      {children}
+    </button>
   )
+}
+
+Button.defaultProps = {
+  disabled: false,
+  type: 'default',
+  size: 'middle'
 }
 export default Button;
