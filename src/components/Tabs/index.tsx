366f838e185d725bdc24e9a0a4c0bce3c89e939b
dongzeping1 <dongzeping1@jd.com>
2023-02-21 19:04:32 +0800

    feat: tabs组件完成

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,30 +1,77 @@
-import React from "react";
+import React,{ useState,useEffect } from "react";
 import classNames from "classnames";
 interface itemProps {
   key:string 
   label:string 
-  content:string
+  content:React.ReactNode,
+  disabled?:boolean
 }
 interface tabsProps {
+  activeKey?:string//当前激活的key
   className?:string
   items?:itemProps[]
+  tabPosition?:'left' | 'top'//tab的位置
+  onTabClick?:(key:string,e:any)=>void
 }
 const Tabs = (props:tabsProps) => {
   const {
     className,
-    items
+    items,
+    activeKey,
+    tabPosition,
+    onTabClick
   } = props;
-  const classes = classNames('tabs',className);
+  const [activedContent,setActivedContent] = useState<any>();
+
+  const [tabKey,setTabKey] = useState(activeKey?activeKey:items![0].key);
+  useEffect(()=>{
+    //get active content
+    const idx:number = items!.findIndex(item=>{
+      return item.key === tabKey;
+    })
+    if(idx===-1) {
+      if(items!.length>0) setActivedContent(items![0]?.content);
+      else setActivedContent(null);    
+    } else {
+      setActivedContent(items![idx]?.content)
+    }
+  },[tabKey,items])
+  const classes = classNames('tabs',className,{
+    [`tabs_${tabPosition}`]:tabPosition
+  });
   return (
     <div 
       className={classes}
     >
-      tabs
+      {/* tab list header */}
+      <div className="tabs_list">
+        
+        {items?.map(item=>(
+          <div 
+            className={item?.disabled ? 'tabs_list_item_disabled':item.key===tabKey ? "tabs_list_item tabs_list_item_actived":"tabs_list_item" }
+            key={item.key}
+            onClick={(e)=>{
+              if(!item.disabled) {
+                setTabKey(item.key);
+                onTabClick && onTabClick(item.key,e);
+              }
+            }}
+          >
+            {item.label}
+          </div>
+        ))}
+      </div>
+
+      {/* content show */}
+      <div className="tabs_content">
+          {activedContent}
+      </div>
     </div>
   )
 }
 
 Tabs.defaultProps = {
-  items:[]
+  items:[],
+  tabPosition:'top'
 }
 export default Tabs;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,30 @@ import SubMenu from "../components/Menu/SubMenu";
 import Process from "../components/Process";
 import Tabs from "../components/Tabs";
 const Home = ()=>{
+  const items = [
+    {
+      key:'key1',
+      label:'label1',
+      content:'content1'
+    },
+    {
+      key:'key2',
+      label:'label2',
+      content:'content2',
+      disabled:true
+    },
+    {
+      key:'key3',
+      label:'label3',
+      content:<div style={{color:'gold'}}>content3</div>
+    }
+  ]
+  const onTabClick = (key:string,e:any) => {
+    console.log('key',key,e)
+  }
   return (
     <div>
-      <Tabs></Tabs>
+      <Tabs items={items} onTabClick={onTabClick} ></Tabs>
     </div>
   )
 }
