2d6c13476f7df6c5fe065956c441a72d27756986
dongzeping1 <dongzeping1@jd.com>
2023-03-17 17:18:01 +0800

    feat: swiper组件完成

--- README.md
+++ README.md
@@ -8,17 +8,17 @@
 7. tabs 切换面板 x
 8. radio 单选 x
 9. pagination 分页器 x
-10. 
-11. message 弹窗消息
-12. modal 弹窗
-13. date 日历
-14. tooltip 提示框
-15. upload 文件上传
-16. select (差一个多选的逻辑)
-17. drawer 抽屉
-18. form 表单
-19. swiper
-20. checkbox 多选-开发中
+10. checkbox 多选 x
+11.  
+12. message 弹窗消息
+13. modal 弹窗
+14. date 日历
+15. tooltip 提示框
+16. upload 文件上传
+17. select -------------(多选优化)
+18. drawer 抽屉
+19. form 表单
+20. swiper---(提前设计)
 
 
 ## blog
---
+++ src/components/Swiper/index.tsx
@@ -0,0 +1,83 @@
+import React,{useEffect,useState} from "react";
+import classNames from "classnames";
+import { divide } from "lodash";
+interface SwiperProps {
+  children?:React.ReactNode,
+  className?:string,
+  duration?:number,
+  onChange?:(e:any)=>void
+}
+const Swiper = (props:SwiperProps) => {
+  const {className,children,duration,onChange} = props;
+  const [activedIndex,setActivedIndex] = useState(0);
+  const [count,setCount] = useState(0);
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+      let index = activedIndex+1;
+      if(index===count) {
+        index = 0;   
+      }
+      setActivedIndex(index);
+      onChange && onChange(index)
+    },duration);
+    return () => {
+      clearInterval(timer);
+    }
+  },[activedIndex])
+
+  useEffect(()=>{
+    if(!children) {
+      console.error('至少传递一个子元素');
+      return;
+    }
+    if(Array.isArray(children)) setCount(children.length);
+    else{
+      setCount(0);
+    }
+  },[])
+
+  //deal with classes
+  const classes = classNames('swiper',className);
+  //click the dot
+  const changeIndex = (index:number) => {
+    setActivedIndex(index);
+    onChange && onChange(index);
+  }
+  //son nodes deal
+  const swiperChilds = (
+    <>
+    {
+      React.Children.map(children,(item:any,index:number)=>{
+        return (
+          <div className={index===activedIndex?'swiper-item swiper-item-actived':'swiper-item'}>
+            {
+              React.cloneElement(item,{
+                index:index
+              })
+            }
+          </div>
+        )
+      })
+    }
+    </>
+  ) 
+  return (
+    <div className={classes}>
+      {swiperChilds}
+      <ul className="swiper-control">
+        {Array.from(new Array(count)).map((item,index)=>(
+          <li 
+            onClick={()=>changeIndex(index)} 
+            key={index}
+            className={index===activedIndex?"swiper-control-item swiper-control-item-actived":"swiper-control-item"}
+          >
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+Swiper.defaultProps = {
+  duration:1500
+}
+export default Swiper;
